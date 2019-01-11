81d93359ff2ebc2689d0933ccd319d4951067654
wanggu <guofu.wang@dianrong.com>
2019-01-11 16:05:40 +0800

    first commit

---
+++ buildSrc/src/main/java/org/example/greeting/ObjProp.java
@@ -0,0 +1,16 @@
+package org.example.greeting;
+
+public class ObjProp {
+
+    private String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+
+}
